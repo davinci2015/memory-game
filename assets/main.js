a2f6302dbfb3e1c1b63b14be8d70219fb7ac8bb8
Danijel <dvincija@foi.hr>
2016-12-17 21:29:54 +0100

    Cards template

---
+++ assets/main.js
@@ -0,0 +1,27 @@
+var BRAINYMO = BRAINYMO || {};
+
+
+BRAINYMO.Game = BRAINYMO.Game || (function() {
+
+        function generateCards() {
+            
+        }
+
+        function openCard() {
+
+        }
+
+        function checkForHit() {
+            
+        }
+
+        return function(config) {
+
+            this.startGame = function() {
+                console.log("Game started");
+                console.log("Configuration: " + config);
+            }
+
+        }
+    
+})();
