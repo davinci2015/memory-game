3533377ab977be4d92ad69ea31851836aab9c62d
Danijel <dvincija@foi.hr>
2016-12-17 21:09:21 +0100

    Main page + js skeleton

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-console.log("Memory");
+var BRAINYMO = BRAINYMO || {};
+
+
+BRAINYMO.Game = BRAINYMO.Game || (function() {
+   
+    return function(config) {
+
+        this.startGame = function() {
+            console.log("Game started");
+            console.log("Configuration: " + config);
+        }
+        
+    }
+    
+})();
