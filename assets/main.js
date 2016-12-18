ba573a520f5151584d4607953f80f60209ae8123
Danijel <dvincija@foi.hr>
2016-12-18 01:44:23 +0100

    Generate cards feature

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,84 @@
 var BRAINYMO = BRAINYMO || {};
 
 
-BRAINYMO.Game = BRAINYMO.Game || (function() {
+BRAINYMO.Game = (function() {
 
-        function generateCards() {
-            
+    return function(config) {
+
+        this.startGame = function() {
+            var card = new BRAINYMO.Card();
+
+            card.attachCardEvent();
+            card.generateCards(config.cards);
+        };
+
+        this.startGame();
+    }
+
+})();
+
+
+
+BRAINYMO.Card = (function () {
+
+    var $cardsContainer = $('.cards-container');
+    var $cardTemplate = $cardsContainer.find('#card-template');
+    
+    function prepareCardTemplate (card) {
+        var template = $cardTemplate.clone().removeAttr('id').removeClass('hide');
+        template.find('.back').css({
+            'background': 'url(' + card.backImg + ') no-repeat center center',
+            'background-size': 'cover'
+        });
+        return template;
+    }
+
+    function shuffleCards(cardsArray) {
+        var currentIndex = cardsArray.length, temporaryValue, randomIndex;
+
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = cardsArray[currentIndex];
+            cardsArray[currentIndex] = cardsArray[randomIndex];
+            cardsArray[randomIndex] = temporaryValue;
         }
 
-        function openCard() {
+        return cardsArray;
+    }
 
-        }
+    function appendCardsToDOM(cards) {
+        var templates = [];
+        var preparedTemplate;
 
-        function checkForHit() {
-            
-        }
+        cards.forEach(function (card) {
+            preparedTemplate = prepareCardTemplate(card);
+            templates.push(preparedTemplate, preparedTemplate.clone());
+        });
 
-        return function(config) {
+        templates = shuffleCards(templates);
 
-            this.startGame = function() {
-                console.log("Game started");
-                console.log("Configuration: " + config);
-            }
+        $cardsContainer.hide().empty();
 
-        }
+        templates.forEach(function(card) {
+            $cardsContainer.append(card);
+        });
+
+        $cardsContainer.fadeIn('slow');
+    }
+    
+    return function() {
+
+        this.attachCardEvent = function() {
+            $cardsContainer.unbind().on('click', '.flip-container', function() {
+                $(this).toggleClass('active');
+            });
+        };
+
+        this.generateCards = function(cards) {
+            appendCardsToDOM(cards);
+        };
+
+    }
     
 })();
